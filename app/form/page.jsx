4fa21e855c71194b28456a3a3b89986e3a46f555
Gammamillard <millard@gammatech.school>
2023-05-16 17:18:05 +0200

    css

---
+++ app/form/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const page = () => {
+    
+
+  return (
+        <div>
+            <form action="">
+                <label htmlFor="">
+                    Usuario
+                </label>
+                    <input  />
+            </form>
+        </div>
+  )
+}
+
+export default page
--- app/utilities/utilities.js
+++ app/utilities/utilities.js
@@ -1,5 +1,7 @@
+
+
 export const getData = async(url) => {
-    const data = await fetch(url)
+    const data = await fetch(url, {next : { revalidate : 60 }})
     return data.json()
 }
 
